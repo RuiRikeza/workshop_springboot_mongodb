019cb94f14c19f4abce9fade846a277500a5bb96
RuiRikeza <ruismagalhaes@gmail.com>
2021-03-23 15:59:25 +0000

    Inserir utilizador

--- src/main/java/com/ruimagalhaes/workshopmongo/services/UserServices.java
+++ src/main/java/com/ruimagalhaes/workshopmongo/services/UserServices.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ruimagalhaes.workshopmongo.domain.User;
+import com.ruimagalhaes.workshopmongo.dto.UserDTO;
 import com.ruimagalhaes.workshopmongo.repository.UserRepository;
 
 @Service
@@ -24,5 +25,12 @@ public class UserServices {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
-	
+	public User insert(User obj) {
+		return repo.insert(obj);
+		
+	}
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+		
+	}
 }
